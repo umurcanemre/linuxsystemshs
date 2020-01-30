894c8312e9b48f7d13faeace13f92cf5260a409d
umurcan emre <ibrahimumurcanemre@hotmail.com>
2020-01-30 14:09:12 +0300

    redis service initialization added to scripts

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,7 @@
 #Should be copied in /etc/profile.d directory so that it is  available for all users 
 
-redis-server /home/umurcan/redis/redis.conf
+# redis service disabled on systemd so redis is only initialized by following command which takes conf file as argument. Also working directory is the same with passed conf file
+redis-server /mnt/Shared/workspace/config/redis/redis.conf
 
 
 
